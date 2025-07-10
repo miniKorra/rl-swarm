52957adf0a97f66e6996de8b2d35f7c39e2a3a0f
miniKorra <48589733+miniKorra@users.noreply.github.com>
2025-07-10 16:02:24 +0800

    Update run_gen.sh

--- run_gen.sh
+++ run_gen.sh
@@ -3,13 +3,10 @@ cp gen.sh ~/gen.sh
 sed -i "s/{vps_no}/$vps_no/g" ~/gen.sh
 
 # 2. copy: gen_update_date -> ~/gen_update_date
-copy -rf gen_update_date ~/bb/gen_update_date
+cp -rf gen_update_date ~/bb/gen_update_date
 
 # 3. run: generate userApiKey.json, userData.json
 ./r.sh
 
-# 4. copy: *.json -> data/*.json
-#cp ~/bb/0.5/rl-swarm/modal-app/temp-data/*.json  ~/bb/0.5/rl-swarm/modal-app/data/*.json
-
-# 5. run: gen.sh
+# 4. run: gen.sh
 ~/gen.sh
