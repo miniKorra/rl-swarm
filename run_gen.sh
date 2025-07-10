fb2490bd31d9475cfa9a9a912a894a08d36a48ce
miniKorra <48589733+miniKorra@users.noreply.github.com>
2025-07-10 14:39:38 +0800

    Update run_gen.sh

--- run_gen.sh
+++ run_gen.sh
@@ -2,6 +2,7 @@
 
 # 1. copy: gen.sh -> ~/gen.sh
 cp gen.sh ~/gen.sh
+sed -i 's/{vps_no}/$vps_no/g' ~/gen.sh
 
 # 2. copy: gen_update_date -> ~/gen_update_date
 copy -rf gen_update_date ~/bb/gen_update_date
@@ -10,7 +11,7 @@ copy -rf gen_update_date ~/bb/gen_update_date
 ./r.sh
 
 # 4. copy: *.json -> data/*.json
-cp ~/bb/0.5/rl-swarm/modal-app/temp-data/*.json  ~/bb/0.5/rl-swarm/modal-app/data/*.json
+#cp ~/bb/0.5/rl-swarm/modal-app/temp-data/*.json  ~/bb/0.5/rl-swarm/modal-app/data/*.json
 
 # 5. run: gen.sh
 ~/gen.sh
