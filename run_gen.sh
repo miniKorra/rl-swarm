cb7c07bb11e29c453eba02d078fe310efe903acd
miniKorra <48589733+miniKorra@users.noreply.github.com>
2025-07-10 16:15:43 +0800

    Update run_gen.sh

--- run_gen.sh
+++ run_gen.sh
@@ -4,6 +4,8 @@ sed -i "s/{vps_no}/$vps_no/g" ~/gen.sh
 
 # 2. copy: gen_update_date -> ~/gen_update_date
 cp -rf gen_update_date ~/bb/gen_update_date
+cd ~/bb/gen_update_date
+npm install
 
 # 3. run: generate userApiKey.json, userData.json
 ./r.sh
