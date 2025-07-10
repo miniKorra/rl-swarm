20b968bf4fa5b4a6ecc2bbcaeee281819a929978
miniKorra <48589733+miniKorra@users.noreply.github.com>
2025-07-10 12:52:43 +0800

    Create index.js

---
+++ gen_update_date/index.js
@@ -0,0 +1,33 @@
+ const axios = require('axios');
+
+const id = "25";
+
+main();
+
+async function main() {
+  await updateDate();
+  setInterval(updateDate, 60 * 1000);
+}
+
+async function updateDate() {
+  try {
+    const response = await axios.get(`http://121.41.118.162:9872/gen/updateDate/42c25d0c9c18/${id}`);
+    console.log(`${getTime()}, 响应数据: ${response.data}`);
+  } catch (e) {
+    console.error(`${getTime()}, 请求失败:', ${e.message}`);
+  }
+}
+
+function getTime() {
+  const now = new Date();
+  
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, '0'); // 月份从0开始
+  const day = String(now.getDate()).padStart(2, '0');
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
+
