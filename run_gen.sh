52f86d9244e956d4ab58771c2156ff1d05518511
miniKorra <48589733+miniKorra@users.noreply.github.com>
2025-07-10 13:35:14 +0800

    Update run_gen.sh

--- run_gen.sh
+++ run_gen.sh
@@ -1,1 +1,16 @@
+# chmod +x *.sh
 
+# 1. copy: gen.sh -> ~/gen.sh
+cp gen.sh ~/gen.sh
+
+# 2. copy: gen_update_date -> ~/gen_update_date
+copy -rf gen_update_date ~/bb/gen_update_date
+
+# 3. run: generate userApiKey.json, userData.json
+./r.sh
+
+# 4. copy: *.json -> data/*.json
+cp ~/bb/0.5/rl-swarm/modal-app/temp-data/*.json  ~/bb/0.5/rl-swarm/modal-app/data/*.json
+
+# 5. run: gen.sh
+~/gen.sh
