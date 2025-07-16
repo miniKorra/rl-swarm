9ef3f7e2173d6dae697de0bfa850d819d5f4cbab
miniKorra <48589733+miniKorra@users.noreply.github.com>
2025-07-16 20:20:03 +0800

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -4,4 +4,8 @@ cd   ~/bb/gen_update_date
 nohup  node index.js {vps_no} {prefix} > output.log 2>&1 &
 
 cd ~/bb/0.5/rl-swarm
+
+# fix: hang for valueError exception
+cp -f game_manager.py  /home/user/.local/lib/python3.12/site-packages/genrl/game/game_manager.py
+
 ./d.sh
